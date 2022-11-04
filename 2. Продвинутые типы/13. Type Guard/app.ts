7db4e2dd1a40accc97dd1cffcdbc7337a489fd42
skaylife <worldskaynet@gmail.com>
2022-11-04 19:22:56 +0700

    git fix

--- 2. Продвинутые типы/13. Type Guard/app.ts
+++ 2. Продвинутые типы/13. Type Guard/app.ts
@@ -28,3 +28,19 @@ function logId(id: string | number) {
 function isString(x: string | number): x is string {
     return typeof x === 'string'
 }
+
+function isAdmin(user: User | Admin): user is Admin {
+    return 'role' in user; 
+}
+
+function isAdminAleternative(user: User | Admin): user is Admin {
+    return (user as Admin).role !== 'undefined';
+}
+
+function setRoleZero(user: User | Admin) {
+    if (isAdmin(user)) {
+        user.role = 0
+    } else {
+        throw new Error('Пользователь не Админ')
+    }
+}
